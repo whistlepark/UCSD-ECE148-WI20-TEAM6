8d06493209f92c79a51ac38ac8f7eedb7f06610c
whistlepark <44822319+whistlepark@users.noreply.github.com>
2020-02-24 15:10:28 -0800

    Add files via upload

---
+++ Object_Detection.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import pyrealsense2 as rs
+import numpy as np
+import pandas as pd
+
+
+#input options in order (right->left): 'camera/rgb', 'camera/depth', 'camera/infra1', 'camera/infra2', 'slam/odom'
+class Object_Detection:
+
+    def __init__(self):
+        pass
+
+    def camera(self):
+
+
+
+

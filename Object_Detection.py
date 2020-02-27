91d8d342353507da0fa9fef253120a2e78ebb1ad
Aaron Hanna <aaronhanna13@gmail.com>
2020-02-27 10:37:53 -0800

    Update readme

--- Object_Detection.py
+++ Object_Detection.py
@@ -4,14 +4,15 @@ import numpy as np
 import pandas as pd
 
 
-#input options in order (right->left): 'camera/rgb', 'camera/depth', 'camera/infra1', 'camera/infra2', 'slam/odom'
+# input options in order (right->left): 'camera/rgb', 'camera/depth', 'camera/infra1', 'camera/infra2', 'slam/odom'
+
 class Object_Detection:
 
-    def __init__(self):
-        pass
+    def __init__(self, rgb=None, depth=None, infra1=None, infra2=None):
+        self.rgb = rgb
+        self.depth = depth
+        self.infra1 = infra1
+        self.infra2 = infra2
 
     def camera(self):
-
-
-
-
+        pass
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # UCSD-ECE148-WI20-TEAM6
 OpenSource Code for Donkey/Realsense Object Detection Interface
+
+## Object Detection
+>Inputs: Camera(rgb, depth, infra)
+>
+>Outputs: Objects data
+
+Ideally, will take in camera data (rgb, depth,infra) as input
+and return objects and their location along with height and distance
+information.
+
+##Object Avoidance
+>Inputs: Objects data from Object_Detection class
+>
+>Outputs: Steering, Throttle
